041d65b826dc6ad59ac047d0deffd63521009f96
Mafizur Alam <mafizuralam000@gmail.com>
2023-08-25 00:27:07 +0530

    sum2number

---
+++ sum2number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+    cout<<"Enter Your First Number: ";
+    cin>>num1;
+    cout<<"Enter Your Second Number: ";
+    cin>>num2;
+    int sum;
+    sum= num1+num2;
+    cout<<"Sum of the Two Numbers is: "<<sum;
+    return 0;
+}
