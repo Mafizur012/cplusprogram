c0f30af8215285f3de5e0097f7d83c85c9ac384f
Mafizur Alam <mafizuralam000@gmail.com>
2023-08-25 00:23:24 +0530

    evenodd

---
+++ evenodd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a Number: ";
+    cin>>n;
+    if(n%2==0){
+        cout<<"This is the Even Number";
+    }
+    else{
+        cout<<"This is the odd Number";
+    }
+    return 0;
+}
